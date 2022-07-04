e1ed3d970971170ef48aad81938f8e8c9fc799b4
mario-gellrich-zhaw <60516564+mario-gellrich-zhaw@users.noreply.github.com>
2022-07-04 13:25:18 +0200

    update summer school FS2022

---
+++ Automated_Crypotocurrency_Trading/Cryptotrading_bot/api_keys.R
@@ -0,0 +1,4 @@
+# API-Keys (never send this in cleartext in an real crypto trading setting!!!)
+api_key    = ''
+secret     = ''
+passphrase = ''
---
+++ Automated_Crypotocurrency_Trading/Cryptotrading_bot/scheduler_example.R
@@ -0,0 +1,3 @@
+sink('C:/Users/gell/Desktop/output.txt', append = TRUE)
+print(Sys.time())
+sink()
